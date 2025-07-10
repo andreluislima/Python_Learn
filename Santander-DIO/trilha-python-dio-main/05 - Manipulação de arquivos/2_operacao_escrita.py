84fa99e9c043653a12ae4175d5a347fe6557a4a3
andecodelima <andrelimag89@gmail.com>
2025-07-10 16:17:18 -0300

    feat: manipul de arquivos

--- Santander-DIO/trilha-python-dio-main/05 - Manipulação de arquivos/2_operacao_escrita.py
+++ Santander-DIO/trilha-python-dio-main/05 - Manipulação de arquivos/2_operacao_escrita.py
@@ -1,6 +1,12 @@
-arquivo = open(
-    "/home/guilherme/Projetos/dio/codigo-fonte/trilha-python-dio/05 - Manipulação de arquivos/teste.txt", "w"
-)
-arquivo.write("Escrevendo dados em um novo arquivo.")
+
+arquivo = open("teste_andre.txt", "w")
+arquivo.write("Escrevendo dados em um novo arquivo de testes")
 arquivo.writelines(["\n", "escrevendo", "\n", "um", "\n", "novo", "\n", "texto"])
+
 arquivo.close()
+# arquivo = open(
+#     "/home/guilherme/Projetos/dio/codigo-fonte/trilha-python-dio/05 - Manipulação de arquivos/teste.txt", "w"
+# )
+# arquivo.write("Escrevendo dados em um novo arquivo.")
+# arquivo.writelines(["\n", "escrevendo", "\n", "um", "\n", "novo", "\n", "texto"])
+# arquivo.close()
