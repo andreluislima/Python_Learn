fe5a9c7ed428a6065d91e6f23a3ad1662d704ead
andecodelima <andrelimag89@gmail.com>
2025-07-11 09:58:21 -0300

    feat: comments

--- Santander-DIO/trilha-python-dio-main/05 - Manipulação de arquivos/6_csv.py
+++ Santander-DIO/trilha-python-dio-main/05 - Manipulação de arquivos/6_csv.py
@@ -3,31 +3,29 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 
-COLUNA_ID = 0
-COLUNA_NOME = 1
+## WRITER
+# try:
+#     with open(ROOT_PATH / "usuarios.csv", "w",  newline="", encoding="utf-8") as arquivo:
+#         escritor = csv.writer(arquivo)
+#         escritor.writerow(["id", "nome"])
+#         escritor.writerow(["1", "Maria"])
+#         escritor.writerow(["2", "João"])
+        
+# except IOError as exc:
+#     print(f'Erro ao criar o arquivo. {exc}')
 
+## READER 1-FORMA
 
-try:
-    with open(ROOT_PATH / "usuarios.csv", "w", newline="", encoding="utf-8") as arquivo:
-        escritor = csv.writer(arquivo)
-        escritor.writerow(["id", "nome"])
-        escritor.writerow(["1", "Maria"])
-        escritor.writerow(["2", "João"])
-except IOError as exc:
-    print(f"Erro ao criar o arquivo. {exc}")
+# try:
+#     with open(ROOT_PATH / "usuarios.csv", "r", encoding="utf-8") as arquivo:
+#         leitor = csv.reader(arquivo)
+#         for row in leitor:
+#             print(row)
+        
+# except IOError as exc:
+#     print(f'Erro ao criar o arquivo. {exc}')
 
-
-try:
-    with open(ROOT_PATH / "usuarios.csv", "r", newline="", encoding="utf-8") as arquivo:
-        leitor = csv.reader(arquivo)
-        for idx, row in enumerate(leitor):
-            if idx == 0:
-                continue
-            print(f"ID: {row[COLUNA_ID]}")
-            print(f"Nome: {row[COLUNA_NOME]}")
-except IOError as exc:
-    print(f"Erro ao criar o arquivo. {exc}")
-
+## READER 2-FORMA
 
 try:
     with open(ROOT_PATH / "usuarios.csv", newline="") as csvfile:
@@ -37,3 +35,5 @@ try:
             print(f"Nome: {row['nome']}")
 except IOError as exc:
     print(f"Erro ao criar o arquivo. {exc}")
+    
+ 
