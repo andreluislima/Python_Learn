ba7f4c746f82c194f60db2f639998e430bcab26f
andreluislima <andrelimag89@gmail.com>
2025-12-24 12:44:36 -0300

    feat: cria endpoint get by id

--- Geek_University/secao03/main.py
+++ Geek_University/secao03/main.py
@@ -16,7 +16,22 @@ cursos = {
     }
 }
 
+@app.get('/curso')
+async def get_cursos():
+    return cursos
+
+@app.get('/curso/{curso_id}')
+async def get_curso(curso_id:int): ## curso_id:int -> Declarando que o valor do id deve ser inteiro. 
+    ## Caso não seja, o fastapi irá tratar o erro.
+    curso = cursos[curso_id]
+    curso.update({"id":curso_id})
+    
+    return curso
+
+
+
+
 if __name__=='__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, debug=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, debug=True, reload=True)
