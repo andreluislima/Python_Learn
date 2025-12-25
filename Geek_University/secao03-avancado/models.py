13b2779c2684648e589aa4e9ad01dd5b3c2e98b3
andreluislima <andrelimag89@gmail.com>
2025-12-25 15:21:14 -0300

    feat: cria rotas

--- Geek_University/secao03-avancado/models.py
+++ Geek_University/secao03-avancado/models.py
@@ -8,3 +8,21 @@ class Curso(BaseModel):
     aulas:int
     horas:int
 
+cursos = {
+    1:{
+        "título":"Programação para leigos",
+        "aulas":111,
+        "horas":58
+    },
+
+    2:{
+        "título":"Algorítmos e lógica de programação",
+        "aulas":87,
+        "horas":67
+    },
+    3:{
+        "título":"Programação em C",
+        "aulas":92,
+        "horas":101
+    }
+}
